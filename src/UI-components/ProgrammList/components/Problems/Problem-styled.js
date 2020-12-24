ab7e5bfd29ac4d01850b7881eac7870588ec8d3c
Kononov <other.world.html@gmail.com>
2020-12-25 00:39:46 +0300

    problem areas

---
+++ src/UI-components/ProgrammList/components/Problems/Problem-styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const ProblemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  align-content: center;
+  grid-gap: 10px;
+  background: #F5F7FA;
+  border-radius: 16px;
+  padding: 0px 24px 30px 24px;
+`
+export const ProblemStyled = styled.div`
+  width: 328px;
+  display: flex;
+  flex-direction: column;
+
+  background: #F5F7FA;
+  border-radius: 16px;
+  padding: 44px 24px 30px 24px;
+`
